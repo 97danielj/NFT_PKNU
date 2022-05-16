029c33fdca039c36e0ca3ea0d59813861542d392
ezard <kg1588@naver.com>
2022-05-16 14:50:51 +0900

    컨트랙트파일 추가 및 수정

---
+++ contracts/NFT_PKNU.sol
@@ -0,0 +1,486 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+
+library Base64 {
+    string internal constant TABLE =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+    function encode(bytes memory data) internal pure returns (string memory) {
+        if (data.length == 0) return "";
+
+        // load the table into memory
+        string memory table = TABLE;
+
+        // multiply by 4/3 rounded up
+        uint256 encodedLen = 4 * ((data.length + 2) / 3);
+
+        // add some extra buffer at the end required for the writing
+        string memory result = new string(encodedLen + 32);
+
+        assembly {
+            // set the actual output length
+            mstore(result, encodedLen)
+
+            // prepare the lookup table
+            let tablePtr := add(table, 1)
+
+            // input ptr
+            let dataPtr := data
+            let endPtr := add(dataPtr, mload(data))
+
+            // result ptr, jump over length
+            let resultPtr := add(result, 32)
+
+            // run over the input, 3 bytes at a time
+            for {
+
+            } lt(dataPtr, endPtr) {
+
+            } {
+                dataPtr := add(dataPtr, 3)
+
+                // read 3 bytes
+                let input := mload(dataPtr)
+
+                // write 4 characters
+                mstore(
+                    resultPtr,
+                    shl(248, mload(add(tablePtr, and(shr(18, input), 0x3F))))
+                )
+                resultPtr := add(resultPtr, 1)
+                mstore(
+                    resultPtr,
+                    shl(248, mload(add(tablePtr, and(shr(12, input), 0x3F))))
+                )
+                resultPtr := add(resultPtr, 1)
+                mstore(
+                    resultPtr,
+                    shl(248, mload(add(tablePtr, and(shr(6, input), 0x3F))))
+                )
+                resultPtr := add(resultPtr, 1)
+                mstore(
+                    resultPtr,
+                    shl(248, mload(add(tablePtr, and(input, 0x3F))))
+                )
+                resultPtr := add(resultPtr, 1)
+            }
+
+            // padding with '='
+            switch mod(mload(data), 3)
+            case 1 {
+                mstore(sub(resultPtr, 2), shl(240, 0x3d3d))
+            }
+            case 2 {
+                mstore(sub(resultPtr, 1), shl(248, 0x3d))
+            }
+        }
+
+        return result;
+    }
+}
+
+library SafeMath {
+    /**
+     * @dev Returns the addition of two unsigned integers, with an overflow flag.
+     *
+     * _Available since v3.4._
+     */
+    function tryAdd(uint256 a, uint256 b)
+        internal
+        pure
+        returns (bool, uint256)
+    {
+        unchecked {
+            uint256 c = a + b;
+            if (c < a) return (false, 0);
+            return (true, c);
+        }
+    }
+
+    /**
+     * @dev Returns the substraction of two unsigned integers, with an overflow flag.
+     *
+     * _Available since v3.4._
+     */
+    function trySub(uint256 a, uint256 b)
+        internal
+        pure
+        returns (bool, uint256)
+    {
+        unchecked {
+            if (b > a) return (false, 0);
+            return (true, a - b);
+        }
+    }
+
+    /**
+     * @dev Returns the multiplication of two unsigned integers, with an overflow flag.
+     *
+     * _Available since v3.4._
+     */
+    function tryMul(uint256 a, uint256 b)
+        internal
+        pure
+        returns (bool, uint256)
+    {
+        unchecked {
+            // Gas optimization: this is cheaper than requiring 'a' not being zero, but the
+            // benefit is lost if 'b' is also tested.
+            // See: https://github.com/OpenZeppelin/openzeppelin-contracts/pull/522
+            if (a == 0) return (true, 0);
+            uint256 c = a * b;
+            if (c / a != b) return (false, 0);
+            return (true, c);
+        }
+    }
+
+    /**
+     * @dev Returns the division of two unsigned integers, with a division by zero flag.
+     *
+     * _Available since v3.4._
+     */
+    function tryDiv(uint256 a, uint256 b)
+        internal
+        pure
+        returns (bool, uint256)
+    {
+        unchecked {
+            if (b == 0) return (false, 0);
+            return (true, a / b);
+        }
+    }
+
+    /**
+     * @dev Returns the remainder of dividing two unsigned integers, with a division by zero flag.
+     *
+     * _Available since v3.4._
+     */
+    function tryMod(uint256 a, uint256 b)
+        internal
+        pure
+        returns (bool, uint256)
+    {
+        unchecked {
+            if (b == 0) return (false, 0);
+            return (true, a % b);
+        }
+    }
+
+    /**
+     * @dev Returns the addition of two unsigned integers, reverting on
+     * overflow.
+     *
+     * Counterpart to Solidity's `+` operator.
+     *
+     * Requirements:
+     *
+     * - Addition cannot overflow.
+     */
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a + b;
+    }
+
+    /**
+     * @dev Returns the subtraction of two unsigned integers, reverting on
+     * overflow (when the result is negative).
+     *
+     * Counterpart to Solidity's `-` operator.
+     *
+     * Requirements:
+     *
+     * - Subtraction cannot overflow.
+     */
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a - b;
+    }
+
+    /**
+     * @dev Returns the multiplication of two unsigned integers, reverting on
+     * overflow.
+     *
+     * Counterpart to Solidity's `*` operator.
+     *
+     * Requirements:
+     *
+     * - Multiplication cannot overflow.
+     */
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a * b;
+    }
+
+    /**
+     * @dev Returns the integer division of two unsigned integers, reverting on
+     * division by zero. The result is rounded towards zero.
+     *
+     * Counterpart to Solidity's `/` operator.
+     *
+     * Requirements:
+     *
+     * - The divisor cannot be zero.
+     */
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a / b;
+    }
+
+    /**
+     * @dev Returns the remainder of dividing two unsigned integers. (unsigned integer modulo),
+     * reverting when dividing by zero.
+     *
+     * Counterpart to Solidity's `%` operator. This function uses a `revert`
+     * opcode (which leaves remaining gas untouched) while Solidity uses an
+     * invalid opcode to revert (consuming all remaining gas).
+     *
+     * Requirements:
+     *
+     * - The divisor cannot be zero.
+     */
+    function mod(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a % b;
+    }
+
+    /**
+     * @dev Returns the subtraction of two unsigned integers, reverting with custom message on
+     * overflow (when the result is negative).
+     *
+     * CAUTION: This function is deprecated because it requires allocating memory for the error
+     * message unnecessarily. For custom revert reasons use {trySub}.
+     *
+     * Counterpart to Solidity's `-` operator.
+     *
+     * Requirements:
+     *
+     * - Subtraction cannot overflow.
+     */
+    function sub(
+        uint256 a,
+        uint256 b,
+        string memory errorMessage
+    ) internal pure returns (uint256) {
+        unchecked {
+            require(b <= a, errorMessage);
+            return a - b;
+        }
+    }
+
+    /**
+     * @dev Returns the integer division of two unsigned integers, reverting with custom message on
+     * division by zero. The result is rounded towards zero.
+     *
+     * Counterpart to Solidity's `/` operator. Note: this function uses a
+     * `revert` opcode (which leaves remaining gas untouched) while Solidity
+     * uses an invalid opcode to revert (consuming all remaining gas).
+     *
+     * Requirements:
+     *
+     * - The divisor cannot be zero.
+     */
+    function div(
+        uint256 a,
+        uint256 b,
+        string memory errorMessage
+    ) internal pure returns (uint256) {
+        unchecked {
+            require(b > 0, errorMessage);
+            return a / b;
+        }
+    }
+
+    /**
+     * @dev Returns the remainder of dividing two unsigned integers. (unsigned integer modulo),
+     * reverting with custom message when dividing by zero.
+     *
+     * CAUTION: This function is deprecated because it requires allocating memory for the error
+     * message unnecessarily. For custom revert reasons use {tryMod}.
+     *
+     * Counterpart to Solidity's `%` operator. This function uses a `revert`
+     * opcode (which leaves remaining gas untouched) while Solidity uses an
+     * invalid opcode to revert (consuming all remaining gas).
+     *
+     * Requirements:
+     *
+     * - The divisor cannot be zero.
+     */
+    function mod(
+        uint256 a,
+        uint256 b,
+        string memory errorMessage
+    ) internal pure returns (uint256) {
+        unchecked {
+            require(b > 0, errorMessage);
+            return a % b;
+        }
+    }
+}
+
+contract NFT_PKNU is ERC721Enumerable, Ownable {
+    using SafeMath for uint256;
+
+    address payable wallet;
+    address payable admin;
+    mapping(uint256 => Card) public cards;
+    uint256 mintingFee = 1000000000000000000;
+
+    //Decimals
+    uint256 private contractDecimals = 10**18;
+
+    event NFTMintedToAddress(address indexed _NFTminter);
+    event AdminUpdated(address _newAdmin);
+    event NFTPriceUpdated(uint256 _newPrice);
+    event WalletUpdated(address _newWallet);
+
+    constructor(address _wallet) ERC721("NFT_PKNU", "PKNU") {
+        require(_wallet != address(0), "Wallet address cannot be 0");
+        wallet = payable(_wallet);
+        admin = payable(msg.sender);
+    }
+
+    using Strings for uint256;
+
+    struct Card {
+        string name;
+        string role;
+        string user_data;
+        string bgColor;
+        string textColor;
+    }
+
+    // public
+    function mint(
+        string memory _name,
+        string memory _role,
+        string memory user_data,
+        string memory bgColor,
+        string memory textColor
+    ) public payable {
+        uint256 supply = totalSupply();
+        Card memory newCard = Card(_name, _role, user_data, bgColor, textColor);
+
+        if (msg.sender != admin) {
+            require(msg.value >= mintingFee, "Please check your balance");
+        }
+        wallet.transfer(address(this).balance);
+
+        cards[supply + 1] = newCard;
+        _safeMint(msg.sender, supply + 1);
+
+        emit NFTMintedToAddress(msg.sender);
+    }
+
+    function buildImage(uint256 _tokenId) public view returns (string memory) {
+        Card memory currentCard = cards[_tokenId];
+        return
+            Base64.encode(
+                bytes(
+                    abi.encodePacked(
+                        '<svg width="750" height="500" xmlns="http://www.w3.org/2000/svg" version="1.2">',
+                        '<rect width="750" height="500" fill="',currentCard.bgColor,'" />',
+                        '<text font-size="50" x="50%" y="40%" dominant-baseline="middle" text-anchor="middle" fill="',currentCard.textColor,'">',
+                        currentCard.name,
+                        "</text>",
+                        '<text font-size="30" x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" fill="',currentCard.textColor,'">',
+                        currentCard.role,
+                        "</text>",
+                        '<text font-size="30" x="50%" y="70%" dominant-baseline="middle" text-anchor="middle" fill="',currentCard.textColor,'">',
+                        currentCard.user_data,
+                        "</text>",
+                        "</svg>"
+                    )
+                )
+            );
+    }
+
+    function buildMetadata(uint256 _tokenId)
+        public
+        view
+        returns (string memory)
+    {
+        Card memory currentCard = cards[_tokenId];
+        return
+            string(
+                abi.encodePacked(
+                    "data:application/json;base64,",
+                    Base64.encode(
+                        bytes(
+                            abi.encodePacked(
+                                '{"name":"',
+                                currentCard.name,
+                                '", "role":"',
+                                currentCard.role,
+                                '", "user data":"',
+                                currentCard.user_data,
+                                '", "image":"',
+                                "data:image/svg+xml;base64,",
+                                buildImage(_tokenId),
+                                '"}'
+                            )
+                        )
+                    )
+                )
+            );
+    }
+
+    function updateNFTPrice(uint256 _newPrice) public onlyOwner {
+        require(msg.sender == admin, "Only owner can execute this function");
+        require(_newPrice > 0, "New Price should be greater than 0");
+        mintingFee = _newPrice;
+        emit NFTPriceUpdated(mintingFee);
+    }
+
+    function updateAdmin(address _newAdmin) public onlyOwner {
+        require(msg.sender == admin, "Only owner can execute this function");
+        require(_newAdmin != address(0), "Address can't be 0x0");
+        admin = payable(_newAdmin);
+        emit AdminUpdated(_newAdmin);
+    }
+
+    function updateWallet(address _newWallet) public onlyOwner {
+        require(msg.sender == admin, "Only owner can execute this function");
+        require(_newWallet != address(0), "Address can't be 0x0");
+        wallet = payable(_newWallet);
+        emit WalletUpdated(_newWallet);
+    }
+
+    function emergencyWitdraw() external payable onlyOwner {
+        require(msg.sender == admin, "Only owner can execute this function");
+        admin.transfer(address(this).balance);
+    }
+
+    function tokenURI(uint256 _tokenId)
+        public
+        view
+        virtual
+        override
+        returns (string memory)
+    {
+        require(
+            _exists(_tokenId),
+            "ERC721Metadata: URI query for nonexistent token"
+        );
+        return buildMetadata(_tokenId);
+    }
+
+    function randomNum(
+        uint256 _mod,
+        uint256 _seed,
+        uint256 _salt
+    ) internal view returns (uint256) {
+        uint256 num = uint256(
+            keccak256(
+                abi.encodePacked(block.timestamp, msg.sender, _seed, _salt)
+            )
+        ) % _mod;
+        return num;
+    }
+
+    function getCurrentPriceOfNFT() external view returns (uint256) {
+        return mintingFee;
+    }
+
+    function getTotalSupplyOfNFT() external view returns(uint256){
+        return totalSupply();
+    }
+}
